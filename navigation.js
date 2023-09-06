89aaa2025f64bc2a5194a06c885ce1485a78b3cd
MoraRMod <ralerwip@gmail.com>
2023-09-05 21:18:38 -0600

    xd

---
+++ navigation.js
@@ -0,0 +1,26 @@
+import React, { Component, SafeAreaView  } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Inicio from './home'
+import Ins from './inscription'
+
+const Stack = createNativeStackNavigator();
+
+export default class Navigation extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Inicio'>
+          <Stack.Screen name='Inicio' component={Inicio} options={{headerShown: false}}/>
+          <Stack.Screen name='Ins' component={Ins}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+}
