8a90ca705bc61efddff278e131372eb35328380d
MoraRMod <ralerwip@gmail.com>
2023-09-05 21:42:49 -0600

    se le da formato

--- navigation.js
+++ navigation.js
@@ -1,14 +1,14 @@
-import React, { Component, SafeAreaView  } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React, {Component, SafeAreaView} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Inicio from './home'
-import Ins from './inscription'
+import Inicio from './home';
+import Ins from './inscription';
 
 const Stack = createNativeStackNavigator();
 
 export default class Navigation extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {};
   }
@@ -16,11 +16,15 @@ export default class Navigation extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Inicio'>
-          <Stack.Screen name='Inicio' component={Inicio} options={{headerShown: false}}/>
-          <Stack.Screen name='Inscription' component={Ins}/>
+        <Stack.Navigator initialRouteName="Inicio">
+          <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="Inscription" component={Ins} />
         </Stack.Navigator>
       </NavigationContainer>
-    )
+    );
   }
 }
