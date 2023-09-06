2369dbb1f0db65c3ebec35abdead5953ede28b3b
MoraRMod <ralerwip@gmail.com>
2023-09-05 20:01:40 -0600

    Archivo base para la navegacion entre pestañas

--- Navigation.js
+++ Navigation.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component, SafeAreaView  } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import INICIO from './Home';
-import INSCRIPCION from './Inscription';
+import Inicio from './Home'
+import Ins from './Inscription'
 
-export default class Navegacion extends Component {
-  constructor(props) {
+const Stack = createNativeStackNavigator();
+
+export default class Navigation extends Component {
+  constructor(props){
     super(props);
     this.state = {};
   }
 
   render() {
-    const Stack = createNativeStackNavigator();
-
     return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="inicio"
-            component={INICIO}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen name="inscripcion" component={INSCRIPCION} />
+        <Stack.Navigator initialRouteName='Inicio'>
+          <Stack.Screen name='Inicio' component={Inicio} options={{headerShown: false}}/>
+          <Stack.Screen name='Ins' component={Ins}/>
         </Stack.Navigator>
       </NavigationContainer>
-    );
+    )
   }
 }
