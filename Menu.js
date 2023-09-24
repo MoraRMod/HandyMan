240bd213a7081526df91159bc259a7e34b2380cf
MoraRMod <ralerwip@gmail.com>
2023-09-24 16:17:40 -0600

    para la validacion

---
+++ Menu.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { View, Text, Alert } from 'react-native';
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style = {{color: "white"}}> Bienvenido: {this.props.route.params.nombre} </Text>
+      </View>
+    );
+  }
+}
+
+intalar: npm install react-native-side-drawer
+
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import MenuDrawer from 'react-native-side-drawer'
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  toggleOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  drawerContent = () => {
+    return (
+      <TouchableOpacity onPress={this.toggleOpen} style={styles.animatedBox}>
+        <Text>Close</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MenuDrawer
+          open={this.state.open}
+          position={'left'}
+          drawerContent={this.drawerContent()}
+          drawerPercentage={45}
+          animationTime={250}
+          overlay={true}
+          opacity={0.4}
+        >
+          <TouchableOpacity onPress={this.toggleOpen} style={styles.body}>
+            <Text>Open</Text>
+          </TouchableOpacity>
+        </MenuDrawer>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    zIndex: 0
+  },
+  animatedBox: {
+    flex: 1,
+    backgroundColor: "#38C8EC",
+    padding: 10
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F04812'
+  }
+})
+
+/*
+Ponerlo en Inicio.js
+
+if(XMLHttpRequest.responseText === "3"){
+  Alert.alert("Error: correo no reconocido.");
+} else {
+  if(xhttp.responseText === "0"){
+    Alert.alert("Error: password erroneo, intenta de nuevo.");
+  } else {
+    NavigationContainer.navigate("Menu", (nombre:xhttp.responseText));
+  }
+}
+*/
