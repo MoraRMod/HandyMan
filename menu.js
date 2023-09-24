5498e2384d5303504660e563153f69c343b0415e
MoraRMod <ralerwip@gmail.com>
2023-09-24 17:24:24 -0600

    white>>>black

--- menu.js
+++ menu.js
@@ -11,7 +11,7 @@ export default class Menu extends Component {
   render() {
     return (
       <View>
-        <Text style={{color: 'white'}}>
+        <Text style={{color: 'black'}}>
           {' '}
           Bienvenido: {this.props.route.params.nombre}{' '}
         </Text>
