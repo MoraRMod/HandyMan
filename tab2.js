91607c02378cf557c8175d1ebc7cfc0c34d12c30
MoraRMod <ralerwip@gmail.com>
2023-09-26 23:21:44 -0600

    export default class tab2 >>> export default class Tab2

---
+++ tab2.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export default class Tab2 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Tab2</Text>
+      </View>
+    );
+  }
+}
