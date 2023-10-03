e3cfda71fe354f08945c06aa3986300ed4714719
MoraRMod <ralerwip@gmail.com>
2023-10-03 11:23:56 -0600

    se elimina libreria inservible

--- menu.js
+++ menu.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import paraMenu from './tab1';
