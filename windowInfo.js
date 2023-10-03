2290beca810e11a5dd7ad40c7f84c17db8a2956b
MoraRMod <ralerwip@gmail.com>
2023-10-03 11:51:50 -0600

    se le agregan estios a la pagina

--- windowInfo.js
+++ windowInfo.js
@@ -1,21 +1,43 @@
 import React, {Component} from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 
 export default class WindowInfo extends Component {
   render() {
     const selectedItem = this.props.route.params.selectedItem;
 
     return (
-      <View>
-        <Text>Nombre: {selectedItem.Nombre}</Text>
-        <Text>Profesión: {selectedItem.Profesion}</Text>
-        <Text>Teléfono: {selectedItem.Telefono}</Text>
+      <View style={styles.flex}>
+        <Text style={styles.text}>{selectedItem.Nombre}</Text>
+        <Text style={styles.text}>{selectedItem.Profesion}</Text>
+        <Text style={styles.text}>{selectedItem.Telefono}</Text>
 
-        <Image
-          source={{uri: selectedItem.Imagen}}
-          style={{width: 200, height: 200}}
-        />
+        <Image source={{uri: selectedItem.Imagen}} style={styles.img} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  img: {
+    width: 300,
+    height: 300,
+    minWidth: 100,
+    minHeight: 100,
+    alignSelf: 'center',
+    marginBottom: 15,
+    borderRadius: 500,
+  },
+  text: {
+    textAlign: 'center',
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  flex: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    alignContent: 'center',
+    justifyContent: 'center',
+    rowGap: 13,
+  },
+});
