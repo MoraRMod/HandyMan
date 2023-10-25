39fb2021c0dea666885fc27853eba34cf1f5e16b
MoraRMod <ralerwip@gmail.com>
2023-10-25 12:12:17 -0600

    onReady

--- tab3.js
+++ tab3.js
@@ -1,6 +1,7 @@
 import {Text, View, StyleSheet} from 'react-native';
 import React, {Component} from 'react';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 export default class Tab3 extends Component {
   render() {
@@ -23,6 +24,45 @@ export default class Tab3 extends Component {
             coordinate={{
               latitude: 20.656206980737775,
               longitude: -103.3252508278986,
+            }}
+          />
+        </MapView>
+
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 20.6270968,
+            longitude: -103.4528802,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.0121,
+          }}
+          showsUserLocation={true}
+          followsUserLocation={true}>
+          <Marker
+            title="CASA"
+            description="Humilde morada"
+            coordinate={{
+              latitude: 20.623959607076984,
+              longitude: -103.45631563059774,
+            }}
+          />
+
+          <MapViewDirections
+            origin={{
+              latitude: 20.656206980737775,
+              longitude: -103.3252508278986,
+            }}
+            destination={{
+              latitude: 20.623959607076984,
+              longitude: -103.45631563059774,
+            }}
+            apikey={AIzaSyDAPdOZDmQDAMa8guBg6Hmi8O5WZy161PA}
+            strokeWidth={3}
+            strokeColor="#C7B8E2"
+            onReady={result => {
+              console.log(`Distancia entre CUCEI y Casa: ${result.distance} km.`);
+              console.log(`Duración aproximada de tiempo: ${result.duration} min.`);
             }}
           />
         </MapView>
